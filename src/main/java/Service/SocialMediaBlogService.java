31cbd54b6ff387ffcb0df13a4e693f8bebf495e3
RAN-co <artyrebrina@gmail.com>
2023-02-07 20:34:29 +0000

    userStory_1 edits

--- src/main/java/Service/SocialMediaBlogService.java
+++ src/main/java/Service/SocialMediaBlogService.java
@@ -4,3 +4,21 @@ import Model.Account;
 import Model.Message;
 import DAO.SocialMediaDAO;
 
+public class SocialMediaBlogService {
+    SocialMediaDAO socialMediaDAO;
+
+    public SocialMediaBlogService(){
+        socialMediaDAO = new SocialMediaDAO();
+    }
+//not necesserly, the code would allow the testing of SocialMediaBlogService independently of SocialMediaDAO
+    public SocialMediaBlogService(SocialMediaDAO socialMediaDAO){
+        this.socialMediaDAO = socialMediaDAO;
+    }
+
+    public Account addAccount(Flight flight){
+        return flightDAO.insertFlight(flight);
+    }
+
+
+
+}
